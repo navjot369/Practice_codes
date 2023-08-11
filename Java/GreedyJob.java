004e431b6faa6016fa622b75b66f0ecfa7220ddc
navjot369 <personal.navjotsingh@gmail.com>
2023-08-11 09:52:08 +0530

    Add code of Job sequencing using Greeedy algorithm.

--- Java/GreedyJob.java
+++ Java/GreedyJob.java
@@ -14,7 +14,7 @@ public class GreedyJob {
     }
 
     public static void main(String args[]) {
-        int JobArray[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+        int JobArray[][] = {{4, 20}, {2, 10}, {1, 40}, {3, 30}};
 
         ArrayList<Job> JobInfo = new ArrayList<>();
 
@@ -22,6 +22,22 @@ public class GreedyJob {
             JobInfo.add(new Job(i, JobArray[i][0], JobArray[i][1]));
         }
 
-        
+        Collections.sort(JobInfo, (j1, j2) -> j2.profit - j1.profit);
+
+        int time = 0;
+        ArrayList<Integer> sequence = new ArrayList<>();
+
+        for(int i = 0; i < JobInfo.size(); i++) {
+            Job curr = JobInfo.get(i);
+            if(curr.deadline > time) {
+                sequence.add(curr.id);
+                time++;
+            }
+        }
+
+        for(int i = 0; i < sequence.size(); i++) {
+            System.out.print(sequence.get(i) + "  ");
+        }
+        System.out.println();
     }
 }
